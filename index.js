82cdde2f47164e7b75c31c6ad1f60e6e9269c928
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-16 19:39:30 -0500

    commit tree 3b41e7ded8c8a6df65eee34a951138cc8973e7e7

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ function addingEventListener() {
     }
 
     input.addEventListener('click', clickAlert);
+    // input.addEventListener('click', function () { // anonymus callback function
+    //     alert('I was clicked!');
+    // });
+    //input.addEventListener('click', () => alert('I was clicked!')); //arrow function
 }
 addingEventListener();
+
+// const divs = document.querySelectorAll('div');
+// console.log(divs[divs.length-1].addEventListener('mouseover', () => console.log('just hovered')));
+// divs[divs.length-2].remove();
